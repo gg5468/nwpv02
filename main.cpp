a5f76f26b8ee18607c7a353078cbf428a9caad21
gg307 <gg307@email.com>
2025-03-23 23:56:45 +0100

    add

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include "nwpwin.h"
 #include "res.h"
 
-// TODO: prepare classes (edit, button, list_box) for child windows
-// TODO: derive from window, override class_name
+class edit : public vsite::nwp::window {
+protected:
+	std::string class_name() override { return "edit"; }
+};
+
+class button : public vsite::nwp::window {
+protected:
+	std::string class_name() override { return "button"; }
+};
+
+class list_box : public vsite::nwp::window {
+protected:
+	std::string class_name() override { return "listbox"; }
+};
 
 class main_window : public vsite::nwp::window
 {
+	edit ed;
+	button btn_a, btn_r;
+	list_box listbox;
 protected:
 	int on_create(CREATESTRUCT* pcs) override;
 	void on_command(int id) override;
@@ -14,39 +29,47 @@ protected:
 
 int main_window::on_create(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
+	listbox.create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, 0, IDC_LB, 40, 10, 200, 200);
+	ed.create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, 0, IDC_EDIT, 250, 10, 300, 20);
+	btn_a.create(*this, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, "Add", IDC_ADD, 300, 50, 100, 30);
+	btn_r.create(*this, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, "Remove", IDC_REMOVE, 300, 80, 100, 30);
+	EnableWindow(btn_r, FALSE);
 	return 0;
 }
 
-void main_window::on_command(int id){
-	switch(id){
-		case ID_FILE_EXIT:
-			// TODO: close main window
-			break;
-		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
-			break;
-		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
-			break;
-		case IDC_REMOVE:
-			// TODO: get listbox selection
-			// TODO: if there is a selection, delete selected string
-			// TODO: disable "Remove" button if listbox is empty
-			break;
+void main_window::on_command(int id) {
+	switch (id) {
+	case ID_FILE_EXIT:
+		on_destroy();
+		break;
+	case ID_HELP_ABOUT:
+		MessageBox(*this, "add adds to list, remove removes from list", "Help", MB_ICONINFORMATION | MB_OK);
+		break;
+	case IDC_ADD:
+		char str[64];
+		GetDlgItemText(*this, IDC_EDIT, str, sizeof(str));
+		SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, 0, (LPARAM)str);
+		EnableWindow(btn_r, TRUE);
+		break;
+	case IDC_REMOVE:
+		int selection = SendMessage(listbox, LB_GETCURSEL, 0, 0);
+		SendMessage(listbox, LB_DELETESTRING, selection, 0);
+
+		int elems = SendMessage(listbox, LB_GETCOUNT, 0, 0);
+		if (!elems) {
+			EnableWindow(btn_r, FALSE);
+		}
+		break;
 	}
 }
 
-void main_window::on_destroy(){
+void main_window::on_destroy() {
 	::PostQuitMessage(0);
 }
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
-	main_window w; 
+	main_window w;
 	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)::LoadMenu(instance, MAKEINTRESOURCE(IDM_V2)));
 	vsite::nwp::set_icons(instance, w, IDI_V2);
 	vsite::nwp::application app;
